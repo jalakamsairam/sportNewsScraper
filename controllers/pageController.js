5c75ab5cb8705f69856d493c7634d1d6567c2d06
jalakamsairam <sairam.jalakamdevarajulu@mavs.uta.edu>
2017-11-29 15:57:09 -0800

    working saved articles page

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -6,51 +6,56 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 
 
-router.get("/",function(req,res){
+router.get("/", function (req, res) {
   console.log("inside the '/'");
-  console.log(req,res);
-  db.Article.find({}).then(function(dbarticle){
-    console.log("the articles from database are",dbarticle);
-    res.render("index",{article:dbarticle});
-    }).catch(function(err){
-      res.json(err);
-    })
- });
+  console.log(req, res);
+  db.Article.find({}).then(function (dbarticle) {
+    console.log("the articles from database are", dbarticle);
+    res.render("index", { article: dbarticle });
+  }).catch(function (err) {
+    res.json(err);
+  })
+});
 
- router.put("/:id",function(req,res){
-    console.log(req.params.id);
-    var query = {_id:(req.params.id)}
-    console.log(query);
-    // console.log(title);
-    db.Article.findByIdAndUpdate(query,{$set:{'isSaved':true}},function(err,data){
-       if (err) {console.log(error)}
-      res.end();
-    })
- })
-
- router.get("/scrapesuccessful",function(req,res){
-   res.render("scrapesuccessful")
- })
-
-router.get("/saved",function(req,res){
-  res.render("savedArticles");
+router.put("/:id", function (req, res) {
+  console.log(req.params.id);
+  var query = { _id: (req.params.id) }
+  console.log(query);
+  // console.log(title);
+  db.Article.findByIdAndUpdate(query, { $set: { 'isSaved': true } }, function (err, data) {
+    if (err) { console.log(error) }
+    res.end();
+  })
 })
 
-router.get("/scrape",function(req,res){
-  axios.get("http://www.sportsonearth.com/").then(function(response){
+router.get("/scrapesuccessful", function (req, res) {
+  res.render("scrapesuccessful")
+})
+
+router.get("/saved", function (req, res) {
+    db.Article.find({isSaved:true}).then(function(dbArticles){
+      console.log("the articles from the database are",dbArticles);
+      res.render("savedArticles",{article:dbArticles})
+    })
+    
+ 
+})
+
+router.get("/scrape", function (req, res) {
+  axios.get("http://www.sportsonearth.com/").then(function (response) {
     var $ = cheerio.load(response.data);
-    $("article h1").each(function(i,element){
+    $("article h1").each(function (i, element) {
       var result = {};
       result.title = $(this).children("a").text();
       result.link = $(this).children("a").attr("href");
       result.isSaved = false;
       // result.story = $(this).children("a").children()
       db.Article.create(result).
-        then(function(dbarticle){
+        then(function (dbarticle) {
           console.log(i)
           // res.render("scrapesuccessful");
           res.redirect("/scrapesuccessful");
-       })//.catch(function(error){
+        })//.catch(function(error){
       //   res.json(error);
       // })
     })
